31014ab0fe9ac444afc53de99fdd4303f5faf6d2
Sebastian Rojas <sebastian.rojas6@unmsm.edu.pe>
2024-10-03 13:03:31 -0500

    structure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-const hola: string = "Hola mundo"
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
-console.log(hola);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`);
+});
